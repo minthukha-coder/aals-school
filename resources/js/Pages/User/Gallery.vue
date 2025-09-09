b394b72c852d60bb7fede61a53480671a46039b5
minthukha-coder <minthukha25122003@gmail.com>
2025-09-09 23:19:12 +0630

    done website

--- resources/js/Pages/User/Gallery.vue
+++ resources/js/Pages/User/Gallery.vue
@@ -18,22 +18,22 @@
                         class="w-full object-cover rounded-md" style="height:280px;object-fit:cover;" />
                 </div>
                 <div>
-                    <img src="../User/images/learning-pathway3.jpg" alt=""
+                    <img src="../User/images/learning-pathway1.jpg" alt=""
                         class="w-full object-cover rounded-md" style="height:280px;object-fit:cover;" />
                 </div>
             </div>
 
             <div class="grid grid-cols-3 gap-3 my-3">
                 <div>
-                    <img src="../User/images/learning-pathway4.jpg" alt=""
+                    <img src="../User/images/learning-pathway2.jpg" alt=""
                         class="w-full object-cover rounded-md" style="height:280px;object-fit:cover;" />
                 </div>
                 <div>
-                    <img src="../User/images/learning-pathway5.jpg" alt=""
+                    <img src="../User/images/learning-pathway1.jpg" alt=""
                         class="w-full object-cover rounded-md" style="height:280px;object-fit:cover;" />
                 </div>
                 <div>
-                    <img src="../User/images/learning-pathway6.jpg" alt=""
+                    <img src="../User/images/learning-pathway2.jpg" alt=""
                         class="w-full object-cover rounded-md" style="height:280px;object-fit:cover;" />
                 </div>
             </div>
