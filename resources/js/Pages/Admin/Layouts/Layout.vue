8114e4d84d9d81842beebeb9628774de9b532076
misu <hsunadikyaw.hndk@gmail.com>
2025-09-09 23:23:03 +0630

    Change font

--- resources/js/Pages/Admin/Layouts/Layout.vue
+++ resources/js/Pages/Admin/Layouts/Layout.vue
@@ -25,6 +25,7 @@ export default {
   display: flex;
   flex-direction: column;
   height: 100vh;
+  font-family: "Inter", sans-serif;
 }
 
 /* The main area below the Header */
@@ -32,6 +33,7 @@ export default {
   display: flex;
   flex: 1;
   overflow: hidden;
+  font-family: "Inter", sans-serif;
 }
 
 .sidebar {
@@ -40,11 +42,14 @@ export default {
   overflow-y: auto;
   border:1px solid #ccc;
   box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.1);
+  font-family: "Inter", sans-serif;
 }
 
 .right-content {
   width: 83%;
   height: 100%;
   overflow-y: auto;
+  font-family: 'Lato', sans-serif;
+  font-family: "Inter", sans-serif;
 }
 </style>
